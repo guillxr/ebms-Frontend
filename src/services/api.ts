455914641b0140e56cac4daade1884a097ef3c97
guillxr . <contaguillxr@gmail.com>
2025-07-17 14:10:17 -0300

    :pencil: feat: Atualizando src/services/api.ts para endpoints atualizados

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,24 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-//   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViNWM4MTVkLTYwYTItNDM3Yy05Yjc4LTA0ZDc5YTk2YWE3YyIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTc1MjcyMDc5NCwiZXhwIjoxNzUyODA3MTk0fQ.y9nnYHKswKXiHnKwJlQuKdZDujfQ0C0ybZGWjdkz9_Y`,
+    "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+function getToken(): string | null {
+  const match = document.cookie.match(new RegExp("(^| )token=([^;]+)"));
+  return match ? match[2] : null;
+}
 
 export default api;
