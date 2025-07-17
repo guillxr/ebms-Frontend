1104ec152a5d28c15eb625cf36a5f180b50fb651
guillxr . <contaguillxr@gmail.com>
2025-07-17 14:10:12 -0300

    :recycle: refactor: Refatorando src/components/nav-user.tsx para nova estrutura

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,16 +1,6 @@
 "use client"
 
-import {
-  IconDotsVertical,
-  IconLogout,
-  IconUserCircle,
-} from "@tabler/icons-react"
-
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
+import { useSidebar } from "@/components/ui/sidebar"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -24,19 +14,37 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar"
+import { useRouter } from "next/navigation"
+import {
+  IconDotsVertical,
+  IconLogout,
+  IconSettings,
+  IconUserCircle,
+} from "@tabler/icons-react"
+import Cookies from "js-cookie"
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
+interface User {
+  name: string
+  email: string
+  avatar: string
+  role: "ADMIN" | "DONOR"
+}
+
+interface NavUserProps {
+  user: User
+}
+
+export function NavUser({ user }: NavUserProps) {
+  const router = useRouter()
   const { isMobile } = useSidebar()
+
+  const handleLogout = () => {
+    Cookies.remove("token")
+    window.location.href = "/login"
+  }
+
+  if (!user) return null
 
   return (
     <SidebarMenu>
@@ -47,19 +55,15 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-              </Avatar>
+              <IconSettings className="h-8 w-8 rounded-lg" />
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="text-muted-foreground truncate text-xs">
-                  {user.email}
-                </span>
+                <span className="font-medium">Configurações</span>
+                <span className="text-muted-foreground truncate text-xs"></span>
               </div>
               <IconDotsVertical className="ml-auto size-4" />
             </SidebarMenuButton>
           </DropdownMenuTrigger>
+
           <DropdownMenuContent
             className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
             side={isMobile ? "bottom" : "right"}
@@ -68,29 +72,28 @@ export function NavUser({
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
-                  <span className="text-muted-foreground truncate text-xs">
-                    {user.email}
-                  </span>
                 </div>
               </div>
             </DropdownMenuLabel>
+
             <DropdownMenuSeparator />
+
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <IconUserCircle />
-                Account
-              </DropdownMenuItem>
+              {user.role === "DONOR" && (
+                <DropdownMenuItem onClick={() => router.push("/profile")}>
+                  <IconUserCircle />
+                  Conta
+                </DropdownMenuItem>
+              )}
             </DropdownMenuGroup>
+
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+
+            <DropdownMenuItem onClick={handleLogout}>
               <IconLogout />
-              Log out
+              Sair
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
